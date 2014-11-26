46c67fab7bd0340f37bdc9b31f0ce89e33769cb3
Kyle Krull <kyle.krull@here.com>
2014-11-26 08:39:15 -0600

    Working through examples on Chapter 2

--- src/main/scala/ChapterTwo.scala
+++ src/main/scala/ChapterTwo.scala
@@ -1,14 +1,32 @@
 import System.out.println
+import java.util
 
 object ChapterTwo {
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(formatFactorial(4))
+    println(formatResult("Fibonacci", 6, fib))
 
-    val n = 5
-    println("fib(%d): %d".format(n, fib(n)))
-    println("fibIter(%d): %d".format(n, fibIter(n)))
+    val arr = Array(1, 6, 3, 4, 5, 2)
+    val findAnInteger = () => {
+      val query = 5
+      val index = firstIndex(arr, (x: Int) => x == query)
+      val findMsg = "First index of %d in %s is %d"
+      println(findMsg.format(query, util.Arrays.toString(arr), index))
+    }
+    findAnInteger()
+
+    val findAnEvenNumber = () => {
+      val index = firstIndex(arr, (x: Int) => x % 2 == 0)
+      val findMsg = "First index of an even number in %s is %d"
+      println(findMsg.format(util.Arrays.toString(arr), index))
+    }
+    findAnEvenNumber()
+
+    println("Powers of 10: %f, %f, %f".format(powerOf10(1), powerOf10(2), powerOf10(3)))
   }
+
+  /* Formatting */
 
   def formatAbs(x: Int): String = {
     val msg = "The absolute value of %d is %d"
@@ -19,6 +37,12 @@ object ChapterTwo {
     val msg = "The factorial of %d is %d"
     msg.format(x, factorial(x))
   }
+
+  def formatResult(name: String, x: Int, f: Int => Int): String = {
+    "The %s of %d is %d".format(name, x, f(x))
+  }
+
+  /* Math */
 
   def abs(x: Int) = {
     if (x < 0) -x
@@ -40,13 +64,34 @@ object ChapterTwo {
     else fib(n - 1) + fib(n - 2)
   }
 
+  //https://mitpress.mit.edu/sicp/chapter1/node13.html
   def fibIter(n: Int): Int = {
     @annotation.tailrec
-    def loop(a: Int, b: Int, count: Int): Int = {
+    def loop(a: Int, b: Int, count: Int): Int =
       if (count == 0) b
       else loop(a + b, a, count - 1)
-    }
 
     loop(1, 0, n)
+  }
+
+  /* Polymorphic functions */
+
+  def firstIndex[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n == as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def powerOf10(x: Double): Double = {
+    val func = partial(Math.pow, 10d)
+    func(x)
+  }
+
+  def partial[A, B, C](f: (A, B) => C, firstArg: A): B => C = {
+    (secondArg: B) => f(firstArg, secondArg)
   }
 }
