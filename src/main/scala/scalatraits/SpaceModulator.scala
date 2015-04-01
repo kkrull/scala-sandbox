9730fc158a57ab678d47f80a79d7933a0833bcd3
Kyle Krull <kyle.krull@here.com>
2015-04-01 09:49:32 -0500

    Default implementation

--- src/main/scala/scalatraits/SpaceModulator.scala
+++ src/main/scala/scalatraits/SpaceModulator.scala
@@ -1,9 +1,9 @@
 package com.github.kkrull.scala.scalatraits
 
 class SpaceModulator extends RayGun {
-  def energize: String = "discombobulate"
+  override def energize: String = "discombobulate"
 }
 
 trait RayGun {
-  def energize: String
+  def energize: String = "bang!"
 }
