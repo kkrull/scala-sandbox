e5e1cbe57cbb31569fe147baa1f4e35639469479
Kyle Krull <kyle.krull@here.com>
2015-04-01 22:05:37 -0500

    Fighting with how to have a hierarchical test context:
    - scalatest does not let you do multiple before blocks in the same test class
    - having the same val shared by two describe blocks is not cool.  it's the same object used throughout both describe blocks

--- src/main/scala/scalatraits/TraitExercises.scala
+++ src/main/scala/scalatraits/TraitExercises.scala
@@ -2,10 +2,12 @@ package com.github.kkrull.scala.scalatraits
 
 import java.awt.geom._
 
-trait RectangleLike {
-  this: Ellipse2D.Double => 
+trait RectangleLike { this: Ellipse2D.Double =>
+  def grow(x: Int, y: Int): Unit = {
+    this.setFrame(this.getX, this.getY, this.getWidth + x, this.getHeight + y)
+  }
 
-    def translate(x: Int, y: Int): Unit = {
-      this.setFrame(this.getX + x, this.getY + y, this.getWidth, this.getHeight )
-    }
+  def translate(x: Int, y: Int): Unit = {
+    this.setFrame(this.getX + x, this.getY + y, this.getWidth, this.getHeight)
+  }
 }
--- src/test/scala/scalatraits/TraitExerciseSpecs.scala
+++ src/test/scala/scalatraits/TraitExerciseSpecs.scala
@@ -8,17 +8,31 @@ class TraitExerciseSpecs extends FunSpec
   with BeforeAndAfter {
 
   describe("RectangleLike") {
-    val subject = new Ellipse2D.Double(5, 10, 20, 30) with RectangleLike
-    
     describe(".translate") {
-      before { subject.translate(10, -10) }
+      val subject = new Ellipse2D.Double(1, 2, 4, 8) with RectangleLike
+      subject.translate(16, -32)
+      
       it("moves the object by the specified number of pixels in each dimension") { 
-        assertResult(15d) { subject.getX }
-        assertResult(0d) { subject.getY }
+        assertResult(17d) { subject.getX }
+        assertResult(-30d) { subject.getY }
       }
       it("maintains the same size") {
-        assertResult(20d) { subject.getWidth }
-        assertResult(30d) { subject.getHeight }
+        assertResult(4d) { subject.getWidth }
+        assertResult(8d) { subject.getHeight }
+      }
+    }
+
+    describe(".grow") {
+      val subject = new Ellipse2D.Double(1, 2, 4, 8) with RectangleLike
+      subject.grow(16, 32)
+
+      it("does not move the object") {
+        assertResult(1) { subject.getX }
+        assertResult(2) { subject.getY }
+      }
+      it("changes the side by the amount specified for each dimension") {
+        assertResult(20) { subject.getWidth }
+        assertResult(40) { subject.getHeight }
       }
     }
   }
