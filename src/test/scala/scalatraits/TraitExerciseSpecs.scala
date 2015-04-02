1a94789d5a1572b54d36238639985d2d89316efb
Kyle Krull <kyle.krull@here.com>
2015-04-01 22:12:01 -0500

    Using matchers

--- src/test/scala/scalatraits/TraitExerciseSpecs.scala
+++ src/test/scala/scalatraits/TraitExerciseSpecs.scala
@@ -12,13 +12,13 @@ class TraitExerciseSpecs extends FunSpec
       val subject = new Ellipse2D.Double(1, 2, 4, 8) with RectangleLike
       subject.translate(16, -32)
       
-      it("moves the object by the specified number of pixels in each dimension") { 
-        assertResult(17d) { subject.getX }
-        assertResult(-30d) { subject.getY }
+      it("moves the object by the specified number of pixels in each dimension") {
+        subject.getX shouldEqual 17d
+        subject.getY shouldEqual -30d
       }
       it("maintains the same size") {
-        assertResult(4d) { subject.getWidth }
-        assertResult(8d) { subject.getHeight }
+        subject.getWidth shouldEqual 4d
+        subject.getHeight shouldEqual 8d
       }
     }
 
@@ -27,12 +27,12 @@ class TraitExerciseSpecs extends FunSpec
       subject.grow(16, 32)
 
       it("does not move the object") {
-        assertResult(1) { subject.getX }
-        assertResult(2) { subject.getY }
+        subject.getX shouldEqual 1
+        subject.getY shouldEqual 2
       }
       it("changes the side by the amount specified for each dimension") {
-        assertResult(20) { subject.getWidth }
-        assertResult(40) { subject.getHeight }
+        subject.getWidth shouldEqual 20
+        subject.getHeight shouldEqual 40
       }
     }
   }
