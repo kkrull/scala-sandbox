19ff0a164385806c138464569ccebf440b4b509f
Kyle Krull <kyle.krull@here.com>
2015-05-06 15:31:25 -0500

    WIP on Chapter 13

--- src/main/scala/col/Collection.scala
+++ src/main/scala/col/Collection.scala
@@ -1,26 +1,33 @@
 package com.github.kkrull.scala.col
 
-object Collection {
-  //def old_indexes(input: String): Map[Char, Set[Int]] = {
-  //  val characterIndexes = input.zipWithIndex
-  //  val init: Map[Char, Set[Int]] = Map()
-  //  characterIndexes.foldLeft(init)((acc: Map[Char, Set[Int]], x: (Char, Int)) => {
-  //    val ch = x._1
-  //    val index = x._2
-  //    val indexSet = acc.getOrElseUpdate(ch, Set[Int]())
-  //    indexSet += index
-
-  //    acc
-  //  })
-  //  //Map('a' -> Set(0, 1))
-  //}
+object MutableCollection {
+  import scala.collection.mutable._
 
   def indexes(input: String): Map[Char, Set[Int]] = {
     val characterIndexes = input.zipWithIndex
     val init: Map[Char, Set[Int]] = Map()
     characterIndexes.foldLeft(init)((acc: Map[Char, Set[Int]], x: (Char, Int)) => {
-      val indexSet = acc.getOrElse(x._1, Set[Int]())
-      acc ++ Map(x._1 -> (indexSet ++ Set(x._2)))
+      val ch = x._1
+      val index = x._2
+      val indexSet = acc.getOrElseUpdate(ch, Set[Int]())
+      indexSet += index
+
+      acc
     })
   }
+}
+
+object ImmutableCollection {
+  import scala.collection.immutable._
+
+  def indexes: Int = 42
+
+  //def indexes(input: String): Map[Char, Set[Int]] = {
+  //  val characterIndexes = input.zipWithIndex
+  //  val init: Map[Char, Set[Int]] = Map()
+  //  characterIndexes.foldLeft(init)((acc: Map[Char, Set[Int]], x: (Char, Int)) => {
+  //    val indexSet = acc.getOrElse(x._1, Set[Int]())
+  //    acc ++ Map(x._1 -> (indexSet ++ Set(x._2)))
+  //  })
+  //}
 }
--- src/test/scala/col/CollectionSpec.scala
+++ src/test/scala/col/CollectionSpec.scala
@@ -2,13 +2,29 @@ package com.github.kkrull.scala.col
 import org.scalatest._
 
 //Chapter 13
-class CollectionSpec extends FunSpec with Matchers {
+class MutableCollectionSpec extends FunSpec with Matchers {
   describe(".indexes") {
     describe("given a string") {
       it("returns a collection index(es) for each character that appears in the string") {
         val index = Map('a' -> Set(0, 3), 'b' -> Set(1), 'c' -> Set(2))
-        Collection.indexes("abca") should equal(index)
+        MutableCollection.indexes("abca") should equal(index)
       }
+    }
+  }
+}
+
+trait GreeterBehaviors { this: FunSpec =>
+  def aGreeter(fn: (String) => String) {
+    it("exists") {
+      fn("bob") should equal("Hello, bob")
+    }
+  }
+}
+
+class ImmutableCollectionSpec extends FunSpec with Matchers {
+  describe("indexes") {
+    it("exists") {
+      ImmutableCollection.indexes
     }
   }
 }
