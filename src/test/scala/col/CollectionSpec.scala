af634288acda8c9f460e172d348de4415668f84e
Kyle Krull <kyle.krull@here.com>
2015-05-07 18:16:45 -0500

    Finally got shared behavior working based upon more abstract types

--- src/test/scala/col/CollectionSpec.scala
+++ src/test/scala/col/CollectionSpec.scala
@@ -1,12 +1,34 @@
 package com.github.kkrull.scala.col
+
+import scala.collection
 import org.scalatest._
 
 //Chapter 13
-class MutableCollectionSpec extends FunSpec with Matchers {
+class MutableCollectionSpec extends FunSpec with IndexerBehaviors with Matchers {
   describe(".indexes") {
+    it should behave like anIndexer(MutableCollection.indexes)
+  }
+}
+
+class ImmutableCollectionSpec extends FunSpec with IndexerBehaviors with Matchers {
+  describe(".indexes") {
+    it should behave like anIndexer(ImmutableCollection.indexes)
+  }
+}
+
+trait IndexerBehaviors { this: FunSpec with Matchers =>
+  def anIndexer(doIndex: (String) => collection.Map[Char, collection.Set[Int]]) = {
+    it("can be invoked") { doIndex("wark!") }
+
+    describe("given an empty string") {
+      it("returns an empty map") {
+        doIndex("") should equal(Map())
+      }
+    }
+
     describe("given a string") {
       it("returns a mapping of characters in the string to the indices at which they appear") {
-        MutableCollection.indexes("abca") should equal(Map(
+        doIndex("abca") should equal(Map(
           'a' -> Set(0, 3), 
           'b' -> Set(1), 
           'c' -> Set(2)
@@ -16,21 +38,3 @@ class MutableCollectionSpec extends FunSpec with Matchers {
   }
 }
 
-class ImmutableCollectionSpec extends FunSpec with Matchers {
-  describe(".indexes") {
-    describe("given an empty string") {
-      it("returns an empty map") {
-        ImmutableCollection.indexes("") should equal(Map())
-      }
-    }
-
-    describe("given a string") {
-      it("returns a mapping of characters in the string to the indices at which they appear") {
-        ImmutableCollection.indexes("aba") should equal(Map(
-          'a' -> Set(0, 2),
-          'b' -> Set(1)
-        ))
-      }
-    }
-  }
-}
