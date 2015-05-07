a5dd9e5ef706c7bd2e00da8aff1daed654edf4d8
Kyle Krull <kyle.krull@here.com>
2015-05-07 13:40:46 -0500

    Having trouble finding the super type for mutable and immutable maps, which makes it hard to use shared behavior for now.

--- src/main/scala/col/Collection.scala
+++ src/main/scala/col/Collection.scala
@@ -20,14 +20,13 @@ object MutableCollection {
 object ImmutableCollection {
   import scala.collection.immutable._
 
-  def indexes: Int = 42
-
-  //def indexes(input: String): Map[Char, Set[Int]] = {
+  def indexes(input: String): Map[Char, Set[Int]] = {
   //  val characterIndexes = input.zipWithIndex
   //  val init: Map[Char, Set[Int]] = Map()
   //  characterIndexes.foldLeft(init)((acc: Map[Char, Set[Int]], x: (Char, Int)) => {
   //    val indexSet = acc.getOrElse(x._1, Set[Int]())
   //    acc ++ Map(x._1 -> (indexSet ++ Set(x._2)))
   //  })
-  //}
+    Map[Char, Set[Int]]()
+  }
 }
--- src/test/scala/col/CollectionSpec.scala
+++ src/test/scala/col/CollectionSpec.scala
@@ -2,9 +2,8 @@ package com.github.kkrull.scala.col
 import org.scalatest._
 
 //Chapter 13
-class MutableCollectionSpec extends FunSpec with IndexerBehavior with Matchers {
+class MutableCollectionSpec extends FunSpec with Matchers {
   describe(".indexes") {
-    it should behave like anIndexer(MutableCollection.indexes)
     describe("given a string") {
       it("returns a collection index(es) for each character that appears in the string") {
         val index = Map('a' -> Set(0, 3), 'b' -> Set(1), 'c' -> Set(2))
@@ -14,29 +13,13 @@ class MutableCollectionSpec extends FunSpec with IndexerBehavior with Matchers {
   }
 }
 
-class ImmutableCollectionSpec extends FunSpec with IndexerBehavior with Matchers {
-  //it should behave like anIndexer()
-}
-
-trait IndexerBehavior { this: FunSpec with Matchers =>
-  def anIndexer(fn: (String) => Map[Char, Set[Int]]) {
+class ImmutableCollectionSpec extends FunSpec with Matchers {
+  describe(".indexes") {
     describe("given a string") {
       it("returns a collection index(es) for each character that appears in the string") {
         val index = Map('a' -> Set(0, 3), 'b' -> Set(1), 'c' -> Set(2))
-        MutableCollection.indexes("abca") should equal(index)
+        ImmutableCollection.indexes("abca") should equal(index)
       }
     }
   }
 }
-
-trait GreeterBehaviors { this: FunSpec with Matchers =>
-  def aGreeter(greetingForName: (String) => String) {
-    it("starts with 1 or more words of greeting") {
-      greetingForName("George") should fullyMatch regex """([^\s]+\s)+George"""
-    }
-    it("end with the given name") {
-      greetingForName("World") should endWith("World")
-    }
-  }
-}
-
