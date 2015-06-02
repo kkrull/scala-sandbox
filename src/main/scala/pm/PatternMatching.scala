107b10f16d987f8190a0af4e23e13c31da6b7e2a
Kyle Krull <kyle.krull@here.com>
2015-06-02 13:32:54 -0500

    Having fun with case classes

--- src/main/scala/pm/PatternMatching.scala
+++ src/main/scala/pm/PatternMatching.scala
@@ -2,10 +2,33 @@ package com.github.kkrull.scala.pm
 
 //Chapter 14
 object PatternMatching {
-  def swapTuple[A, B](pair: (A, B)): (B, A) = pair match { case (a, b) => (b, a) }
+  def swapTuple(pair: (Int, Int)): (Int, Int) = {
+    pair match { case (a, b) => (b, a) } 
+  }
 
   def swapArray(array: Array[Int]) = array match { 
     case Array(a: Int, b: Int, rest @ _*) => Array(b, a) ++ rest 
   }
+}
+
+abstract class Item {
+  def price: Double 
+}
+
+case class Product(description: String, price: Double) extends Item
+case class Multiple(count: Int, item: Item) extends Item {
+  def price = count * item.price 
+}
+
+abstract class BinaryTree {
+  def sum: Int
+}
+
+case class Leaf(value: Int) extends BinaryTree {
+  def sum = value
+}
+
+case class Node(left: BinaryTree, right: BinaryTree) extends BinaryTree {
+  def sum = left.sum + right.sum
 }
 
--- src/test/scala/pm/PatternMatchingSpec.scala
+++ src/test/scala/pm/PatternMatchingSpec.scala
@@ -30,5 +30,46 @@ class PatternMatchingSpec extends FunSpec with Matchers {
       }
     }
   }
+
+  describe("Multiple") {
+    it("is a valid scala case class") {
+      Multiple(2, Product("Toaster", 29.95))
+    }
+
+    describe(".price") {
+      it("multitplies the count by the unit price") {
+        val items = Multiple(2, Product("Toaster", 20))
+        items.price should equal(40)
+      }
+    }
+  }
+
+  describe("BinaryTree") {
+    it("has leaves") {
+      val leaf = Leaf(1)
+    }
+
+    it("has nodes with 2 children") {
+      val node = Node(Leaf(1), Leaf(2))
+    }
+
+    describe(".sum") {
+      describe("given a Leaf") {
+        it("returns that leaf's value") {
+          val leaf = Leaf(42)
+          leaf.sum should equal(42)
+        }
+      }
+
+      describe("given a node") {
+        it("returns the sum of the left and right subtrees") {
+          val node = Node(
+            Node(Leaf(1), Leaf(2)),
+            Node(Leaf(3), Leaf(4)))
+          node.sum should equal(10)
+        }
+      }
+    }
+  }
 }
 
