d5a49a931f4e009a62a521a0e39a61ee752a19c9
Kyle Krull <kdkrull@gmail.com>
2025-03-03 14:39:09 -0600

    Name consistently

--- scalatest/src/test/scala/greeter/GreeterFunSuite.scala
+++ scalatest/src/test/scala/greeter/GreeterFunSuite.scala
@@ -3,10 +3,6 @@ import org.scalatest._
 import funsuite._
 
 class HelloSuite extends AnyFunSuite {
-  def greeting(): String = {
-    "Hello World!"
-  }
-
   test("greeting should address the whole world") {
     val subject = new Greeter()
     val result = subject.makeGreeting()
