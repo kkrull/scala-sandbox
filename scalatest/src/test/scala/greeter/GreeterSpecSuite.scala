83f1408625fdf6d341f3deb9f7eca6e94653bf0c
Kyle Krull <kdkrull@gmail.com>
2025-03-03 14:55:04 -0600

    Life subject

--- scalatest/src/test/scala/greeter/GreeterSpecSuite.scala
+++ scalatest/src/test/scala/greeter/GreeterSpecSuite.scala
@@ -5,15 +5,15 @@ import greeter.Greeter
 
 class GreeterSpec extends AnyFunSpec {
   describe("Greeter") {
+    val subject = new Greeter()
+
     describe("#makeGreeting") {
       it("should greet the world, given no name") {
-        val subject = new Greeter()
         val result = subject.makeGreeting()
         assert(result == "Hello World!")
       }
 
       it("should greet a person by name, given a name") {
-        val subject = new Greeter()
         val result = subject.makeGreeting("George")
         assert(result == "Hello George!")
       }
