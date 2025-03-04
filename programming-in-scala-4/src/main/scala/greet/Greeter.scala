dd70753b1c2eadc9244c5dd846c4fc73f19267dd
Kyle Krull <kdkrull@gmail.com>
2025-03-04 14:38:08 -0600

    Add companion object with main

--- programming-in-scala-4/src/main/scala/greet/Greeter.scala
+++ programming-in-scala-4/src/main/scala/greet/Greeter.scala
@@ -1,5 +1,12 @@
 package greet
 
+object Greeter {
+  def main(args: Array[String]): Unit = {
+    val greeter = new Greeter()
+    println(greeter.makeGreeting())
+  }
+}
+
 class Greeter {
   def makeGreeting(): String = {
     "Hello World!"
