d42cad5b9e6c5f9a4380204d83a280063d6cf81f
Kyle Krull <kdkrull@gmail.com>
2025-03-04 13:54:47 -0600

    Add mutable spec

--- programming-in-scala-4/src/test/scala/collection/SetSpecSuite.scala
+++ programming-in-scala-4/src/test/scala/collection/SetSpecSuite.scala
@@ -1,5 +1,6 @@
 package collection
 
+import scala.collection.mutable
 import org.scalatest._
 import org.scalatest.matchers.should._
 import funspec._
@@ -11,6 +12,14 @@ class SetSpec extends AnyFunSpec with Matchers {
       val other = one + 3
       other shouldEqual(Set(1, 2, 3))
       one shouldEqual(Set(1, 2))
+    }
+  }
+
+  describe("mutable.Set") {
+    it("is a mutable variety of the Set trait") {
+      val set = mutable.Set(1, 2)
+      set += 3
+      set shouldEqual(Set(1, 2, 3))
     }
   }
 }
