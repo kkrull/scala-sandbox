696ff16a037440b055196c66c275a76f9d58d870
Kyle Krull <kdkrull@gmail.com>
2025-03-07 14:05:51 -0600

    Introduce data structure

--- programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
+++ programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
@@ -1,16 +1,21 @@
 package roman
 
 object RomanNumeral {
+  val NumberToLetter = Map(
+    (5 -> "V"),
+    (1 -> "I")
+  )
+
   def convert(number: Int): String = {
-    if(number == 5)
-      "V"
-    else if(number == 1)
-      "I"
-    else if(number == (5+1))
-      convert(number - 1) + convert(1)
-    else if(number == (1+1))
-      convert(number - 1) + convert(1)
-    else
-      "bogus"
+    NumberToLetter get(number) match {
+      case Some(letter) => letter
+      case None =>
+        if(number == (5+1))
+          convert(number - 1) + convert(1)
+        else if(number == (1+1))
+          convert(number - 1) + convert(1)
+        else
+          "bogus"
+    }
   }
 }
