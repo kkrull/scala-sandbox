9addc15b528a51141704f39c90ffdb55d3316b3a
Kyle Krull <kdkrull@gmail.com>
2025-03-07 13:59:02 -0600

    Teach append I

--- programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
+++ programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
@@ -4,8 +4,8 @@ object RomanNumeral {
   def convert(number: Int): String = {
     if(number == 5)
       "V"
-    else if(number == 2)
-      "I" + "I"
+    else if(number == (1+1))
+      convert(number - 1) + convert(1)
     else if(number == 1)
       "I"
     else
