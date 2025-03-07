41c423f6987da4380f6b4dc1fc8cbf8cb8bde687
Kyle Krull <kdkrull@gmail.com>
2025-03-07 14:02:05 -0600

    Change order of case analysis

--- programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
+++ programming-in-scala-4/src/main/scala/roman/RomanNumeral.scala
@@ -2,14 +2,14 @@ package roman
 
 object RomanNumeral {
   def convert(number: Int): String = {
-    if(number == (5+1))
-      convert(number - 1) + convert(1)
-    else if(number == 5)
+    if(number == 5)
       "V"
-    else if(number == (1+1))
-      convert(number - 1) + convert(1)
     else if(number == 1)
       "I"
+    else if(number == (5+1))
+      convert(number - 1) + convert(1)
+    else if(number == (1+1))
+      convert(number - 1) + convert(1)
     else
       "bogus"
   }
