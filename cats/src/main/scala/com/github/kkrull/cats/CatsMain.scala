caf6d075aa2e4f4db90ecef4c980acf5e5bb489d
Kyle Krull <kdkrull@gmail.com>
2025-08-07 09:51:29 -0500

    Await one result

--- cats/src/main/scala/com/github/kkrull/cats/CatsMain.scala
+++ cats/src/main/scala/com/github/kkrull/cats/CatsMain.scala
@@ -2,16 +2,19 @@ package com.github.kkrull.cats
 
 import cats.syntax.all._
 
-import scala.concurrent.{ExecutionContext, Future}
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 case class User(id: Int, name: String)
 
 object CatsMain extends App {
   implicit val ec: ExecutionContext = ExecutionContext.global
+  val timeout: Duration = Duration.create(1, TimeUnit.SECONDS)
 
   def findUser(name: String)(implicit ec: ExecutionContext): Future[Option[User]] = {
     Future({
-      println(s"[findUser] lookup '${name}''")
+      println(s"[findUser] lookup '${name}'")
       name match {
         case "fails" =>
           throw new RuntimeException(s"failed to look up user: ${name}")
@@ -24,7 +27,7 @@ object CatsMain extends App {
 
 
   val fails = findUser("fails")
-  println(s"result of fails: ${fails}")
+  println(s"result of fails: ${Await.result(fails, timeout)}")
 
   val notFound = findUser("notfound")
   println(s"result of notFound: ${notFound}")
