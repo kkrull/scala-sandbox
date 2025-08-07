af2049c7846ceb7b6fb6d4a0ec5041c9ded9fc9c
Kyle Krull <kdkrull@gmail.com>
2025-08-07 09:43:03 -0500

    Start computing futures

--- cats/src/main/scala/com/github/kkrull/cats/CatsMain.scala
+++ cats/src/main/scala/com/github/kkrull/cats/CatsMain.scala
@@ -2,25 +2,33 @@ package com.github.kkrull.cats
 
 import cats.syntax.all._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 case class User(id: Int, name: String)
 
 object CatsMain extends App {
-  def findUser(name: String): Future[Option[User]] = {
-    name match {
-      case "fails" => Future.failed(new RuntimeException(s"failed to look up user: ${name}"))
-      case "notfound" => Future.successful(None)
-      case "reachable" => Future.successful(Some(User(1, "reachable")))
-    }
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
+  def findUser(name: String)(implicit ec: ExecutionContext): Future[Option[User]] = {
+    Future({
+      println(s"[findUser] lookup '${name}''")
+      name match {
+        case "fails" =>
+          throw new RuntimeException(s"failed to look up user: ${name}")
+        case "notfound" => Option.empty[User]
+        case "reachable" => Some(User(1, "reachable"))
+        case _ => Option.empty[User]
+      }
+    })
   }
 
+
   val fails = findUser("fails")
-  println(s"fails: ${fails}")
+  println(s"result of fails: ${fails}")
 
   val notFound = findUser("notfound")
-  println(s"notFound: ${notFound}")
+  println(s"result of notFound: ${notFound}")
 
   val reachable = findUser("reachable")
-  println(s"reachable: ${reachable}")
+  println(s"result of reachable: ${reachable}")
 }
