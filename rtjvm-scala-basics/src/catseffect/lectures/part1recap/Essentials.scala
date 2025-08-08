e2889facca9423ac923a8e2fe7ac88fa3b07765a
Kyle Krull <kdkrull@gmail.com>
2025-08-08 10:11:14 -0500

    m24 on recap

--- rtjvm-scala-basics/src/catseffect/lectures/part1recap/Essentials.scala
+++ rtjvm-scala-basics/src/catseffect/lectures/part1recap/Essentials.scala
@@ -1,5 +1,14 @@
 package catseffect.lectures.part1recap
 
-object Essentials {
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext
 
+object Essentials {
+  val incrementer: Int => Int = x => x + 1
+
+  implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+
+  def main(args: Array[String]): Unit = {
+    println(s"Hello World!")
+  }
 }
