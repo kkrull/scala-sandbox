c68e116c97b0a16bc63381678ffac9e050840e3e
Kyle Krull <kdkrull@gmail.com>
2025-08-08 17:21:25 -0500

    Recap lecture 1

--- rtjvm-scala-basics/src/catseffect/lectures/part1recap/Essentials.scala
+++ rtjvm-scala-basics/src/catseffect/lectures/part1recap/Essentials.scala
@@ -1,7 +1,8 @@
 package catseffect.lectures.part1recap
 
 import java.util.concurrent.Executors
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object Essentials {
   val incrementer: Int => Int = x => x + 1
@@ -10,5 +11,11 @@ object Essentials {
 
   def main(args: Array[String]): Unit = {
     println(s"Hello World!")
+
+    val answer = Future { 42 }
+    answer.onComplete {
+      case Success(value) => println(s"Received answer: ${value}")
+      case Failure(exception) => println(s"Failed with exception: ${exception}")
+    }
   }
 }
