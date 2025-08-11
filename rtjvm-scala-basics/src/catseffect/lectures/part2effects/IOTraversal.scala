d04051d27da46b3d340b902143b66d68ff27f8a9
Kyle Krull <kdkrull@gmail.com>
2025-08-11 17:56:45 -0500

    Run sequentially

--- rtjvm-scala-basics/src/catseffect/lectures/part2effects/IOTraversal.scala
+++ rtjvm-scala-basics/src/catseffect/lectures/part2effects/IOTraversal.scala
@@ -2,6 +2,7 @@ package catseffect.lectures.part2effects
 
 import cats.Traverse
 import cats.effect.{IO, IOApp}
+import utils._
 
 import scala.concurrent.Future
 import scala.util.Random
@@ -9,26 +10,35 @@ import scala.util.Random
 object IOTraversal extends IOApp.Simple {
 
   import scala.concurrent.ExecutionContext.Implicits.global
-  val workLoad: List[String] = List("rock your socks off", "less talk, more rock", "holy guacamole")
 
   def heavyComputation(string: String): Future[Int] = Future {
     Thread.sleep(Random.nextInt(1000))
     string.split(" ").length
   }
 
+  val workLoad: List[String] = List("rock your socks off", "less talk, more rock", "holy guacamole")
+
   def clunkyFutures(): Unit = {
     val futures: List[Future[Int]] = workLoad.map(heavyComputation)
     futures.foreach(_.foreach(println))
   }
 
+  import cats.instances.list._
+  val listTraverse = Traverse[List]
+
   def traverseFutures(): Unit = {
-    import cats.instances.list._
-    val listTraverse = Traverse[List]
     val singleFuture: Future[List[Int]] = listTraverse.traverse(workLoad)(heavyComputation)
     singleFuture.foreach(println)
   }
 
+  def computeAsIO(string: String): IO[Int] = IO {
+    Thread.sleep(Random.nextInt(1000))
+    string.split(" ").length
+  }.debug
 
+  val ios: List[IO[Int]] = workLoad.map(computeAsIO)
+  val singleIO: IO[List[Int]] = listTraverse.traverse(workLoad)(computeAsIO)
 
-  override def run: IO[Unit] = ???
+  override def run: IO[Unit] =
+    singleIO.map(_.sum).debug.void
 }
