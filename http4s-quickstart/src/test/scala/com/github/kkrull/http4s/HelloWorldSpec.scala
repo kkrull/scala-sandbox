3b7d7fed3d553b6b551a97c46f2601b6eb30bff1
Kyle Krull <kdkrull@gmail.com>
2025-08-12 11:51:43 -0500

    Rename for clarity

--- http4s-quickstart/src/test/scala/com/github/kkrull/http4s/HelloWorldSpec.scala
+++ http4s-quickstart/src/test/scala/com/github/kkrull/http4s/HelloWorldSpec.scala
@@ -6,7 +6,6 @@ import org.http4s._
 import org.http4s.implicits._
 
 class HelloWorldSpec extends CatsEffectSuite {
-
   test("HelloWorld returns status code 200") {
     assertIO(retHelloWorld.map(_.status), Status.Ok)
   }
@@ -16,8 +15,10 @@ class HelloWorldSpec extends CatsEffectSuite {
   }
 
   private[this] val retHelloWorld: IO[Response[IO]] = {
-    val getHW = Request[IO](Method.GET, uri"/hello/world")
+    val getHello = Request[IO](Method.GET, uri"/hello/world")
     val helloWorld = HelloWorld.impl[IO]
-    Http4sQuickstartRoutes.helloWorldRoutes(helloWorld).orNotFound(getHW)
+    Http4sQuickstartRoutes
+      .helloWorldRoutes(helloWorld)
+      .orNotFound(getHello)
   }
 }
