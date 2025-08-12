992730dc734f1f1eabbbc972ef9534e95f35c0c4
Kyle Krull <kdkrull@gmail.com>
2025-08-12 08:32:20 -0500

    Make dependencies lazy values

--- http4s/project/Dependencies.scala
+++ http4s/project/Dependencies.scala
@@ -1,13 +1,13 @@
 import sbt.*
 
 object Dependencies {
-  val http4s = "org.http4s" %% "http4s-core" % "0.23.15"
-  val http4sDsl = "org.http4s" %% "http4s-dsl" % http4s.revision
-  val http4sEmberServer = "org.http4s" %% "http4s-ember-server" % http4s.revision
+  lazy val http4s = "org.http4s" %% "http4s-core" % "0.23.15"
+  lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % http4s.revision
+  lazy val http4sEmberServer = "org.http4s" %% "http4s-ember-server" % http4s.revision
 
-  val scalaMock = "org.scalamock" %% "scalamock" % "7.4.0"
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
+  lazy val scalaMock = "org.scalamock" %% "scalamock" % "7.4.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
 
-  val sttp = "com.softwaremill.sttp.client3" %% "core" % "3.11.0"
-  val sttpClient = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % sttp.revision
+  lazy val sttp = "com.softwaremill.sttp.client3" %% "core" % "3.11.0"
+  lazy val sttpClient = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % sttp.revision
 }
