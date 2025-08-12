cb70493c384d22b21bb161183a0d5427071cb8e3
Kyle Krull <kdkrull@gmail.com>
2025-08-12 14:53:55 -0500

    Clean up test some more

--- http4s-quickstart/src/test/scala/com/github/kkrull/http4s/greet/HelloWorldSpec.scala
+++ http4s-quickstart/src/test/scala/com/github/kkrull/http4s/greet/HelloWorldSpec.scala
@@ -8,27 +8,31 @@ import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class HelloWorldSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
-  def retHelloWorld(name: String = "world"): IO[Response[IO]] = {
-    val getHello = Request[IO](Method.GET, uri"/hello".addSegment(name))
-    val helloWorld = HelloWorldService.impl[IO]
-    HelloRoutes
-      .helloWorldRoutes(helloWorld)
-      .orNotFound(getHello)
-  }
-
   "HelloWorld" - {
+    val subject: HttpApp[IO] = HelloRoutes
+      .helloWorldRoutes(HelloWorldService.impl[IO])
+      .orNotFound
+
     "GET /hello/:name" - {
       "responds 200 OK" in {
-        retHelloWorld()
+        doRequest(helloRequest())
           .map(_.status)
           .asserting(_ shouldBe Status.Ok)
       }
 
       "responds with a greeting object for the given name" in {
-        retHelloWorld(name = "world")
+        doRequest(helloRequest(name = "world"))
           .flatMap(_.as[String])
           .asserting(_ shouldEqual "{\"message\":\"Hello world\"}")
       }
     }
+
+    def doRequest(request: Request[IO]): IO[Response[IO]] =
+      subject(request)
+
+    def helloRequest(name: String = "world"): Request[IO] = Request[IO](
+      Method.GET,
+      uri"/hello".addSegment(name)
+    )
   }
 }
