0b8887690d29e61c5e598066218ec7d855059699
Kyle Krull <kdkrull@gmail.com>
2025-08-13 10:26:47 -0500

    Rename fields in routes class

--- http4s-quickstart/src/main/scala/com/github/kkrull/http4s/greet/HelloRoutes.scala
+++ http4s-quickstart/src/main/scala/com/github/kkrull/http4s/greet/HelloRoutes.scala
@@ -6,15 +6,15 @@ import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
 object HelloRoutes {
-  def make[F[_]: Sync](H: HelloWorldService[F]): HttpRoutes[F] = {
+  def make[F[_]: Sync](service: HelloWorldService[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F] {}
 
     import dsl._
     HttpRoutes.of[F] { case GET -> Root / "hello" / name =>
       for {
-        greeting <- H.greet(Name(name))
-        resp <- Ok(greeting)
-      } yield resp
+        greeting <- service.greet(Name(name))
+        response <- Ok(greeting)
+      } yield response
     }
   }
 }
